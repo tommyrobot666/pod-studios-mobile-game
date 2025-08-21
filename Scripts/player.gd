9f38e2e2a82417b40176c37033a2d41ab8d5a4e6
Amin Djellab <its.amin.djellab@gmail.com>
2025-08-21 17:14:42 +0100

    Upload first game prototype

---
+++ Scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+
+@export var mouse_sensitivity = 0.5
+var camera_rotation = Vector3()
+
+@onready var head = $Camera3D 
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) 
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= event.relative.x * 0.5
+		
+	elif event.is_action_pressed("ui_cancel"): 
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) 
+
+
+func _physics_process(delta):
+	
+	var SPEED = 20.0
+	
+	var input_direction_2D = Input.get_vector(
+		"move_left", "move_right", "move_forward","move_back"
+	)
+	var input_direction_3D = Vector3(
+		input_direction_2D.x, 0.0, input_direction_2D.y
+	)
+	
+	var direction = transform.basis * input_direction_3D
+	
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
+	
+	velocity.y -= 20.0 * delta
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = 25.0 
+	elif Input.is_action_just_released("jump") and velocity.y > 0.0:
+		velocity.y = 0.0
+
+	move_and_slide()
